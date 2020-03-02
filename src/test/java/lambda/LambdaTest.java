3b30bbfbe07144409017bd0d35294e402a35fd74
qianwu2 <qianwu2@ap.xom.com>
2020-03-02 16:57:06 +0800

    lambda [] add lambda test

---
+++ src/test/java/lambda/LambdaTest.java
@@ -0,0 +1,79 @@
+package lambda;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LambdaTest {
+
+
+  @Test
+  public void should_apply_to_interface_with_single_abstract_method() {
+    StringFunc lambda = () -> "Hello";
+
+    // TODO: please modify the following code to pass the test
+    // <--start
+    final String expect = null;
+    // --end-->
+
+    assertEquals(expect, lambda.getString());
+  }
+
+  @Test
+  public void should_be_able_to_bind_to_instance_method() {
+    // TODO: please bind lambda to instanceMethod.
+    // <--start
+    StringFunc lambda = null;
+    // --end-->
+
+    assertEquals("instanceMethod", lambda.getString());
+  }
+
+  @Test
+  public void should_be_able_to_bind_to_static_method() {
+    // TODO: please bind lambda to staticMethod
+    // <--start
+    StringFunc lambda = null;
+    // --end-->
+
+    assertEquals("staticMethod", lambda.getString());
+  }
+
+  @Test
+  public void should_bind_to_constructor() {
+    // TODO: please bind lambda to constructor of ArrayList<Integer>
+    // <--start
+    GenericFunc<ArrayList<Integer>> lambda = null;
+    // --end-->
+
+    ArrayList<Integer> value = lambda.getValue();
+
+    assertEquals(0, value.size());
+  }
+
+  @Test
+  public void should_capture_variable_in_a_closure() {
+    int captured = 5;
+
+    StringFunc lambda = () -> captured + " has been captured.";
+
+    final String message = lambda.getString();
+
+    // TODO: please modify the following code to pass the test
+    // <--start
+    final String expected = null;
+    // --end-->
+
+    assertEquals(expected, message);
+  }
+
+  private static String staticMethod() {
+    return "staticMethod";
+  }
+
+  private String instanceMethod() {
+    return "instanceMethod";
+  }
+
+}
