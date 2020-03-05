e00c95e2534554298aee837835bfeb88592963eb
qianwu2 <qianwu2@ap.xom.com>
2020-03-05 13:36:26 +0800

    lambda [] add implementation

--- src/test/java/lambda/LambdaTest.java
+++ src/test/java/lambda/LambdaTest.java
@@ -1,5 +1,6 @@
 package lambda;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +11,11 @@ public class LambdaTest {
 
   @Test
   public void should_apply_to_interface_with_single_abstract_method() {
-    StringFunc lambda = () -> "Hello";
+    StringFunction lambda = () -> "Hello";
 
     // TODO: please modify the following code to pass the test
     // <--start
-    final String expect = null;
+    final String expect = "Hello";
     // --end-->
 
     assertEquals(expect, lambda.getString());
@@ -24,7 +25,7 @@ public class LambdaTest {
   public void should_be_able_to_bind_to_instance_method() {
     // TODO: please bind lambda to instanceMethod(use method reference).
     // <--start
-    StringFunc lambda = null;
+    StringFunction lambda = this::instanceMethod;
     // --end-->
 
     assertEquals("instanceMethod", lambda.getString());
@@ -34,7 +35,7 @@ public class LambdaTest {
   public void should_be_able_to_bind_to_static_method() {
     // TODO: please bind lambda to staticMethod(use method reference)
     // <--start
-    StringFunc lambda = null;
+    StringFunction lambda = LambdaTest::staticMethod;
     // --end-->
 
     assertEquals("staticMethod", lambda.getString());
@@ -44,7 +45,12 @@ public class LambdaTest {
   public void should_bind_to_constructor() {
     // TODO: please bind lambda to constructor of List<Integer>(use method reference)
     // <--start
-    GenericFunc<List<Integer>> lambda = null;
+    GenericFunction<List<Integer>> lambda = new GenericFunction<List<Integer>>() {
+      @Override
+      public List<Integer> getValue() {
+        return new ArrayList<Integer>();
+      }
+    };
     // --end-->
 
     List<Integer> value = lambda.getValue();
@@ -56,13 +62,13 @@ public class LambdaTest {
   public void should_capture_variable_in_a_closure() {
     int captured = 5;
 
-    StringFunc lambda = () -> captured + " has been captured.";
+    StringFunction lambda = () -> captured + " has been captured.";
 
     final String message = lambda.getString();
 
     // TODO: please modify the following code to pass the test
     // <--start
-    final String expected = null;
+    final String expected = "5 has been captured.";
     // --end-->
 
     assertEquals(expected, message);
