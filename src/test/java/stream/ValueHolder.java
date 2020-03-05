1820e9f3ad3a8c2e30237559e63b51d8dc55aaec
qianwu2 <qianwu2@ap.xom.com>
2020-03-05 14:03:25 +0800

    stream [] add stream test

---
+++ src/test/java/stream/ValueHolder.java
@@ -0,0 +1,13 @@
+package stream;
+
+public class ValueHolder<T> {
+    private T value;
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
