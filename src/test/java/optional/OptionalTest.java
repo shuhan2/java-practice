b0ae3f334087b2876cc9b613cabcc85b1703c875
qianwu2 <qianwu2@ap.xom.com>
2020-03-08 09:32:00 +0800

    add optionalTest answer

--- src/test/java/optional/OptionalTest.java
+++ src/test/java/optional/OptionalTest.java
@@ -2,6 +2,7 @@ package optional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -17,13 +18,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class OptionalTest {
   @Test
   void should_throws_if_get_value_of_empty_optional() {
-    // TODO: please create an empty optional and specify the concrete exception type.
-    // <--start
-    Optional<String> empty = Optional.empty();
-    Class errorType = null;
-    // --end-->
 
-    assertThrows(NullPointerException.class, empty::get);
+    Optional<String> empty = Optional.empty();
+    Class errorType = NoSuchElementException.class;
+
+    assertThrows(errorType, empty::get);
   }
 
   @Test
@@ -38,37 +37,32 @@ public class OptionalTest {
     assertEquals("great", nonEmptyResult);
   }
 
-  @SuppressWarnings({"ConstantConditions", "unused"})
   @Test
   void should_be_able_to_throw_for_empty_optional() {
     Optional<String> empty = Optional.empty();
 
-    // TODO: In the `Runnable` object. Please throw IllegalStateException when `empty` is not present.
-    // <--start
-    Runnable emptyRunnable = null;
+    Runnable emptyRunnable = () -> {
+      if (!empty.isPresent()) {
+        throw new IllegalStateException();
+      }
+    };
     // --end-->
 
     assertThrows(IllegalStateException.class, emptyRunnable::run);
   }
 
-  @SuppressWarnings({"MismatchedQueryAndUpdateOfCollection", "ConstantConditions"})
   @Test
   void should_process_value_if_present() {
     Optional<String> optional = Optional.of("word");
     List<String> result = new ArrayList<>();
 
-    // TODO: please add the upper-cased value to result if `optional` is present in `Consumer<Optional<String>>`
-    // TODO: implementation.
-    // <--start
-    Consumer<Optional<String>> optionalConsumer = null;
-    // --end-->
+    Consumer<Optional<String>> optionalConsumer = item -> item.ifPresent(s -> result.add(s.toUpperCase()));
 
     optionalConsumer.accept(optional);
 
     assertEquals("WORD", result.get(0));
   }
 
-  @SuppressWarnings({"ConstantConditions", "MismatchedQueryAndUpdateOfCollection"})
   @Test
   void should_map_value_if_present() {
     Optional<String> optional = Optional.of("word");
@@ -76,11 +70,8 @@ public class OptionalTest {
 
     List<String> result = new ArrayList<>();
 
-    // TODO: The `Function<Optional<String>, Optional<Boolean>>` will map `Optional<String>` to `Optional<Boolean>`
-    // TODO: please add the upper-cased value to `result` list if optional is present. Then return the boolean
-    // TODO: mapping result of `result.add`.
-    // <--start
-    Function<Optional<String>, Optional<Boolean>> mapping = null;
+
+    Function<Optional<String>, Optional<Boolean>> mapping = stringOptional -> stringOptional.map(item -> result.add(item.toUpperCase()));
     // --end-->
 
     Optional<Boolean> mappingResult = mapping.apply(optional);
@@ -91,7 +82,6 @@ public class OptionalTest {
     assertFalse(emptyMappingResult.isPresent());
   }
 
-  @SuppressWarnings("ConstantConditions")
   @Test
   void should_flat_map_optional_value_do_chain_method() {
     Stream<YieldOptional> emptyStream = Stream.of(1, 2, 3)
@@ -101,11 +91,7 @@ public class OptionalTest {
         .filter(i -> i > 1)
         .map(i -> new YieldOptional());
 
-    // TODO: The `findFirstAndGet` interface will find first item in stream. If it can be found, map it with
-    // TODO: `YieldOptional.get` method. Otherwise, returns empty Optional.
-    // <--start
-    Function<Stream<YieldOptional>, Optional<String>> findFirstAndGet = null;
-    // --end-->
+    Function<Stream<YieldOptional>, Optional<String>> findFirstAndGet = yieldOptionalStream -> yieldOptionalStream.findFirst().flatMap(YieldOptional::get);
 
     Optional<String> emptyStreamResult = findFirstAndGet.apply(emptyStream);
     Optional<String> nonEmptyStreamResult = findFirstAndGet.apply(nonEmptyStream);
@@ -116,10 +102,8 @@ public class OptionalTest {
   }
 
   private static <T> T getValue(Optional<T> optional, T defaultValue) {
-    // TODO: please implement the following method to pass the test
-    // <--start
-    throw new NotImplementedException();
-    // --end-->
+
+    return optional.orElseGet(() -> defaultValue);
   }
 
   class YieldOptional {
